8843e4b030d67f6a559a416612725e5ebad5f086
chenxing <ennuwork@outlook.com>
2020-04-13 14:40:09 +0800

    修改的项目

--- src/main/java/com/eunco/demonstrate/controller/UserController.java
+++ src/main/java/com/eunco/demonstrate/controller/UserController.java
@@ -5,6 +5,7 @@ import com.eunco.demonstrate.entity.UserInfoEntity;
 import com.eunco.demonstrate.service.UserService;
 import com.eunco.demonstrate.util.UniteMessage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,7 +30,7 @@ public class UserController {
      * @param session
      * @return
      */
-    @PostMapping(value = "/login.do")
+    @GetMapping(value = "/login.do")
     public UniteMessage userLogin(String username, String password, HttpSession session) {
         UniteMessage uniteMessage = userService.userLogin(username, password);
         // 判断是否登录成功, 成功则保存session
--- src/main/java/com/eunco/demonstrate/vo/UserInfoVO.java
+++ src/main/java/com/eunco/demonstrate/vo/UserInfoVO.java
@@ -12,25 +12,15 @@ import lombok.Data;
 public class UserInfoVO {
 
     private Integer id;
-
     private String username;
-
     private String userpic;
-
     private String email;
-
     private String phone;
-
     private String question;
-
     private String answer;
-
     private Integer role;
-
     private String createTime;
-
     private String updateTime;
-
     private String ip;
 
 }
